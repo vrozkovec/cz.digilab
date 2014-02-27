e7c69340bd435115b34be02ac363b28c4acaa8ec
vrozkovec <vit.rozkovec@gmail.com>
2014-02-27 11:51:24 +0100

    added info

--- www/system/config.php
+++ www/system/config.php
@@ -1,3 +1,8 @@
 <?php
+
+//pro spravnou funkci je potreba mit zapisovatelny adresar v $savePath 
+//a take adresar /www/system/server/files/
+
+
 global $savePath;
 $savePath = "/data/tmp/digilab/";
